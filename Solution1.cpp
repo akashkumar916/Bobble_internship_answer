bc82a2453e35fae0d675ea16ce63a14c6c45772d
Akash kumar <ak0771218@gmail.com>
2020-02-05 21:46:00 +0530

    Update Solution1.cpp

--- Solution1.cpp
+++ Solution1.cpp
@@ -1,15 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long 
+
+//recursive function to calculate all permutation
 void recursive_cal(int i, string s, vector< vector<char> > v)
 {
     if (i >= (int) v.size()) {
         cout << s << ", ";
         return;
     }
- 	vector<char>::iterator it;
-   for (it=v[i].begin();it!=v[i].end();it++) {
-        s.push_back(*it);
+ 	
+   for (char c:v[i]) {
+        s.push_back(c);
         recursive_cal(i + 1, s, v);
         s.pop_back();
     }
@@ -20,33 +22,42 @@ int main(){
 
 
 	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	freopen("input.txt","r",stdin);
-	freopen("output.txt","w",stdout);
- 	vector< vector<char> >v;
- 	string line,word;
-   	fstream fin, fout; 
- 	fin.open("in.csv", ios::in); //Reading the input.csv file 
+	
+    cin.tie(0);
 
-   	    //reading every line by line
+	//freopen("input.txt","r",stdin);
+	//freopen("output.txt","w",stdout);
+
+ 	vector< vector<char> >v// storing dynamic array
+
+  	string line,word;
+
+
+  	fstream fin, fout; 
+ 	fin.open("input.csv", ios::in); //Reading the input.csv file 
+
+  	    //reading every line by line
  	
-   	 	while( getline(fin, line)){
+  	while(getline(fin, line)){
   	 	        stringstream s(line); 
-  	 	        //reading specefic line
-  	 	        vector<char>temp;
-  	 	        int i=0;
-         while (getline(s, word, ',')) { //cout<<word[2]<<" "<<word.size()<<endl;
-     			   if(i==0){temp.push_back(word[1]);i=1;} //only string allowed
-     			   else {temp.push_back(word[2]);}
+  	 	        
+  	 	        vector<char>temp; // will contains all character of first line of csv file
+  		        
+  		        int i=0;
+        		while (getline(s, word, ',')) { 
+
+     			   	if(i==0){temp.push_back(word[1]);i=1;} //if first word of line  then store 2nd character
+
+     			   	else {temp.push_back(word[2]);} //else store 3rd character
+
      			}
-  	v.push_back(temp);
+  				v.push_back(temp);
   	}
   	
-  	//for(int i=0;i<temp.size();i++)cout<<temp[i]<<" ";
-  	//	cout<<endl;
-  	//}
+  	
 
+  	//recursive solution to printing all permutation
    recursive_cal(0, "", v);
 
-    return 0;
+   return 0;
 }
